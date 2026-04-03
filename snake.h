a8ad4a2da3c74a87340d06c7b8a5c1164504fb81
Aravind <jaravind2007@gmail.com>
2026-04-04 00:44:43 +0530

    updated snake.h file

--- snake.h
+++ snake.h
@@ -1,79 +1,89 @@
-#ifndef SNAKE_H   
+#ifndef SNAKE_H
 #define SNAKE_H
 
-#include <gtk/gtk.h>   
-#include <stdlib.h>    
-#include <time.h>      
-#include <string.h>    
+#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
+/* grid settings */
+#define GRID_COLS 25
+#define GRID_ROWS 25
 
-#define GRID_COLS   25          
-#define GRID_ROWS   25          
-#define CELL_SIZE   24         
-#define BORDER_PAD  12
+/* size of each box */
+#define CELL_SIZE 24
 
-/* Derived window dimensions  */
-#define WINDOW_W  (GRID_COLS * CELL_SIZE + BORDER_PAD * 2)
-#define WINDOW_H  (GRID_ROWS * CELL_SIZE + BORDER_PAD * 2 + 60)
+/* space around game */
+#define BORDER_PAD 12
 
-//    GAME BEHAVIOUR CONSTANTS
+/* window size (calculated from grid) */
+#define WINDOW_W (GRID_COLS * CELL_SIZE + BORDER_PAD * 2)
+#define WINDOW_H (GRID_ROWS * CELL_SIZE + BORDER_PAD * 2 + 60)
 
-#define MAX_SNAKE    (GRID_COLS * GRID_ROWS)  
-#define INITIAL_LEN  4                        
-#define TICK_MS      140                      
-#define SCORE_PER_FOOD 10                     
+/* game settings */
+#define MAX_SNAKE (GRID_COLS * GRID_ROWS)
+#define INITIAL_LEN 4
+#define TICK_MS 140
+#define SCORE_PER_FOOD 10
 
-/* DIRECTION ENUM */
+/* direction of snake */
 typedef enum {
-    DIR_UP,     
-    DIR_DOWN,   
-    DIR_LEFT,   
-    DIR_RIGHT   
+    DIR_UP,
+    DIR_DOWN,
+    DIR_LEFT,
+    DIR_RIGHT
 } Direction;
 
-/* POINT STRUCT */
+/* simple point (x, y) */
 typedef struct {
-    int x;   /* Column index  */
-    int y;   /* Row index   */
+    int x;
+    int y;
 } Point;
 
-/* GAME STATE STRUCT */
+/* main game data */
 typedef struct {
-    Point     body[MAX_SNAKE]; 
-    int       length;          
-    Direction dir;             
-    Direction next_dir;        
-    Point     food;            
-    int       score;           
-    int       high_score;      
-    gboolean  running;         
-    gboolean  game_over;       
-    gboolean  paused;          /
+
+    Point body[MAX_SNAKE];  
+    int length;             
+
+    Direction dir;           
+    Direction next_dir;     
+
+    Point food;             
+
+    int score;
+    int high_score;
+
+    gboolean running;       
+    gboolean game_over;     
+    gboolean paused;         
+
 } GameState;
 
-/* GLOBAL VARIABLE DECLARATIONS */
-extern GameState  gs;       
-extern GtkWidget *canvas;   
-extern guint      timer_id; 
+/* global variables */
+extern GameState gs;
+extern GtkWidget *canvas;
+extern guint timer_id;
 
-/* 
-   FUNCTION PROTOTYPES
-   Declarations of all functions defined across modules.
-    */
+/* functions */
 
-/*   structs.c   */
+/* start / reset game */
 void game_init(void);
+
+/* create food randomly */
 void place_food(void);
 
-/*  logic.c   */
+/* game loop */
 gboolean game_tick(gpointer data);
 
-/*   ui.c   */
+/* draw everything */
 gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer data);
-void     draw_rounded_rect(cairo_t *cr, double x, double y,
-                            double w, double h, double r);
 
-/*  input.c   */
-gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer data);
-
-#endif /* SNAKE_H */
+/* small helper for UI */
+void draw_rounded_rect(cairo_t *cr, double x, double y,
+                          double w, double h, double r);
+/* keyboard input */
+gboolean on_key_press(GtkWidget *widget,
+    GdkEventKey *event,
+   gpointer data);
+#endif
